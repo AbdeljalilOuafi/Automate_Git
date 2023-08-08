9ac0e3f3c021ef28dabbff153781a1cb95fe8213
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2023-08-08 02:29:12 +0100

    created style.css and index.html files

---
+++ README.md
@@ -0,0 +1,4 @@
+## Automate Git
+# A simple bash file that automates the git add . git commit git push commands 
+
+
---
+++ git.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git add .
+
+echo 'Enter the commit message:'
+read commitMessage
+
+git commit -m "$commitMessage"
+
+git push origin $branch
+
+read
+
+
